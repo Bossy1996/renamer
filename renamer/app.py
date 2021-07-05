3fed89ed4913d0b900b9564f48b1c71b43fff1ed
Jon Andoni Galdos <jonandonigaldos1996@gmail.com>
2021-07-05 19:48:09 +0200

    app skeleton

---
+++ renamer/app.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# renamer/__init__.py
+
+"""This module provides the renamer application."""
+
+import sys
+
+from PyQt5.QtWidgets import QtApplication
+
+from .views import Window
+
+def main():
+    # Create the application 
+    app = QtApplication(sys.argv)
+    # Create and show the main window
+    win = Window()
+    win.show()
+    # Run the event loop
+    sys.exit(app.exec())
