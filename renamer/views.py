5a1e34b8c8545a71ba9665c714324ac199517c73
Jon Andoni Galdos <jonandonigaldos1996@gmail.com>
2021-07-05 19:47:51 +0200

    renamer main window

---
+++ renamer/views.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# renamer/__init__.py
+
+"""This module provides the renamer main window."""
+
+from PyQt5.QtWidgets import QWidget
+
+from .ui.window import Ui_Window
+
+class Window(QWidget, Ui_Window):
+    def __init__(self) -> None:
+        super().__init__()
+        self._setupUi()
+
+    def _setupUi(self):
+        self.setupUi()
