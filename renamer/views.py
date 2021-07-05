11da60236b2e4c25637a66608fee414c53429df4
Jon Andoni Galdos <jonandonigaldos1996@gmail.com>
2021-07-05 20:20:31 +0200

    window actions

--- renamer/views.py
+++ renamer/views.py
@@ -24,7 +24,34 @@ FILTERS = ";;".join(
 class Window(QWidget, Ui_Window):
     def __init__(self) -> None:
         super().__init__()
+        self._files = deque()
+        self._filesCount = len(self._files)
         self._setupUi()
+        self._connectSignalsSlots()
 
     def _setupUi(self):
         self.setupUi()
+
+    def _connectSignalsSlots(self):
+        self.loadFilesButton.clicked.connect(self.loadFiles)
+
+    def loadFiles(self):
+        self.dstFileList.clear()
+        if self.dirEdit.text():
+            initDir = self.dirEdit.text()
+        else:
+            initDir = str(Path.home())
+        
+        files, filter = QFileDialog.getOpenFileName(
+            self, "Choose Files to rename", initDir, filter=FILTERS
+        )
+
+        if len(files) > 0:
+            fileExtension = filter[filter.index("*"): -1]
+            self.extensionLabel.setText(fileExtension)
+            srcDirName = str(Path(files[0]).parent)
+            self.dirEdit.setText(srcDirName)
+            for file in files:
+                self._files.append(Path(file))
+                self.srcFileList.addItem(file)
+            self._filesCount = len(self._files)
