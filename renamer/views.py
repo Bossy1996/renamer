d88e169ffaa62088f3f18bb378ab30dfd3981ddb
Jon Andoni Galdos <jonandonigaldos1996@gmail.com>
2021-07-05 21:02:01 +0200

    renamed thread and update renamer state

--- renamer/views.py
+++ renamer/views.py
@@ -5,9 +5,12 @@
 
 from collections import deque
 from pathlib import Path
+from sys import prefix
 
+from PyQt5.QtCore import QThread
 from PyQt5.QtWidgets import QFileDialog,QWidget
 
+from .rename import Renamer
 from .ui.window import Ui_Window
 
 FILTERS = ";;".join(
@@ -34,6 +37,7 @@ class Window(QWidget, Ui_Window):
 
     def _connectSignalsSlots(self):
         self.loadFilesButton.clicked.connect(self.loadFiles)
+        self.renameFilesButton.clicked.connect(self.renameFiles)
 
     def loadFiles(self):
         self.dstFileList.clear()
@@ -55,3 +59,30 @@ class Window(QWidget, Ui_Window):
                 self._files.append(Path(file))
                 self.srcFileList.addItem(file)
             self._filesCount = len(self._files)
+
+    def renameFiles(self):
+        self._runRenamerThread()
+    
+    def _runRenamerThread(self):
+        prefix = self.prefixEdit.text()
+        self._thread = QThread()
+        self._renamer = Renamer(
+            files=tuple(self._files),
+            prefix=prefix
+        )
+        self._renamer.moveToThread(self._thread)
+        # Rename
+        self._thread.started.connect(self._renamer.renameFiles)
+        # Update State
+        self._thread.started.connect(self._updateStateWhenFileRenamed)
+        # Clean up
+        self._renamer.finished.connect(self._thread.quit)
+        self._renamer.finished.connect(self._renamer.deleteLater)
+        self._renamer.finished.connect(self._thread.deleteLater)
+        # Run the thread
+        self._thread.start()
+
+    def _updateStateWhenFileRenamed(self, newFile):
+        self._files.popleft()
+        self.srcFileList.takenItem(0)
+        self.dstFileList.addItem(str(newFile))
